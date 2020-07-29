53645c3327ab89be3308212f2d18001a1dda452f
Maria Vazquez Martinez <maria.vazquezmartinez3@gmail.com>
2020-07-29 18:51:57 -0400

    finished quiz functions

--- js_files/questions.js
+++ js_files/questions.js
@@ -9,60 +9,60 @@
 		question: "DACA provides a pathway to citizenship.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "blah",
+		info: "DACA does not provide a path to citizenship for recipients. It’s very difficult for DACA recipients to become citizens actually because most are not eligible for a green card and a in order to become a citizen, one must first have a green card for 5 years. The primary way to get a green card is by something called Family Reunification which means an immediate relative or spouse who is a citizen must sponsor you and most people with DACA do not have that.",
   };
 	let three = {
 		question: "The wall is being built on the US-Mexican border",
 		answers: ["True", "False"],
 		correctAnswer: "True",
-		info: "blah",
+		info: "This is true. The wall is also being built over sacred sites in Arizona and cutting through native land.",
   };
 	let four ={
 		question: "The US grants asylum to more refugees than any other country.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "blah",
+		info: "From 1982 - 2017, the United States led the world in admitting refugees. However, Trump lowered the refugee ceiling down to 45,000 in 2017, but only admitted 22,500. For comparison,from January 2017 to September 2019 admitted 76,200. In 2016 alone, 85,500 refugees were admitted into the United States",
   };
 	let five = {
 		question: "We should prioritize citizenship to immigrants who can contribute to the economy.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "blah",
+		info: "Nope nope my bro: let's not base human life on their economic worth. This would be agreeing that people are only worth as much money they can make.",
   };
 	let six = {
 		question: "President Obama deported more immigrants than any other president to date",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "blah",
+		info: "Although the jury is still out for Trump, Obama deported somewhere around 2.8 &mdash; 3.4 million people during his presidency. Although there is conflicting information on the DHS and ICE websites, in their first three years of presidency, Obama deported about 400,000 more than Trump.",
   };
 	let seven = {
 		question: "More immigrants fall below the official poverty threshold than US citizens.",
 		answers: ["True", "False"],
 		correctAnswer: "True",
-		info: "blah",
+		info: "15% percent of immigrants were poor (as defined, family incomes below the official poverty threshold), compared to 13% of the U.S.-born population.",
   };
 	let eight = {
 		question: "ICE and DHS have been around for a long time.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "blah",
+		info: "The Department of Homeland Security was signed into law by President George W. Bush on November 25, 2002. ICE was established in March of 2003. The first step towards helping immigrants is to recognize that not all the current institutions have been around for a long time. Although the United States has a history of xenophobia, we need to stop normalizing the establishment of these new institutions, along with the long-standing ones."
   };
 	let nine = {
 		question: "3,780 people in ICE custody have contracted COVID-19.",
 		answers: ["True", "False"],
 		correctAnswer: "True",
-		info: "blah",
+		info: "As of July 28, 2020, ICE is reporting 3,835 cases of COVID-19 in facilities holding immigrants around the country.",
   };
 	let ten = {
 		question: "There are more Hispanics and Latinos in the US that are immigrants than nativeborns.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "blah",
+		info: "Many of the U.S. Hispanics are native born. Of the 59.8 million people in 2018 who self-identified as Hispanic or Latino, 33 percent (19.8 million) were immigrants and 67 percent (40 million) were native born.",
   };
 	let eleven = {
 		question: "It's easy to gain citizenship the right way.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "blah",
+		info: "To become a citizen, immigrants must first become a permanent resident (AKA green card holder) which is already very hard to get and can take a lot of time. Only after living in the US for five consecutive years as a green card holder may they apply for naturalization, which is the process of becoming a citizen. They must be 18 or older, be able to read, write, and speak basic English, understand the US constitution, have knowledge of the US history and government, and take the oath of allegiance to the US. Also to apply for naturalization they must pay a free of $725. This process can last 6-14 months. They must then complete an interview and test. The overall process is very very long and costly.",
   };
 let questions = [one, two, three, four, five, six, seven, eight, nine, ten, eleven];
