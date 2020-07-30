1dd9f4138c56c28bb1c0cf779adee8d2460ab480
Maria Vazquez Martinez <maria.vazquezmartinez3@gmail.com>
2020-07-30 18:26:17 -0400

    more styling updates

--- js_files/questions.js
+++ js_files/questions.js
@@ -3,19 +3,19 @@
 		question: "20% of immigrants are reported to be White.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "It is much larger than 20%; 46 percent of immigrants reported their race as single-race White, 27 percent as Asian, 10 percent as Black, and 15 percent as some other race. About 2 percent reported having two or more races",
+		info: "There are more than 20%; 46% of immigrants reported their race as single-race White, 27% as Asian, 10% as Black, and 15% as some other race. About 2% reported having two or more races",
 	};
 	let two = {
 		question: "DACA provides a pathway to citizenship.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "DACA does not provide a path to citizenship for recipients. It’s very difficult for DACA recipients to become citizens actually because most are not eligible for a green card and a in order to become a citizen, one must first have a green card for 5 years. The primary way to get a green card is by something called Family Reunification which means an immediate relative or spouse who is a citizen must sponsor you and most people with DACA do not have that.",
+		info: 'DACA does not provide a path to citizenship for recipients. It’s very difficult for DACA recipients to become citizens since most are not eligible for a green card. In order to become a citizen, one must first have a green card for 5 years. The primary way to get a green card is through "Family Reunification," which means an immediate relative or spouse who is a citizen must sponsor you and most people with DACA do not have that.',
   };
 	let three = {
-		question: "The wall is being built on the US-Mexican border",
+		question: "The wall is being built on the US-Mexico border",
 		answers: ["True", "False"],
 		correctAnswer: "True",
-		info: "This is true. The wall is also being built over sacred sites in Arizona and cutting through native land.",
+		info: "Part of the wall is being built over O'odham land in Arizona and desecrating sacred sites.",
   };
 	let four ={
 		question: "The US grants asylum to more refugees than any other country.",
@@ -27,7 +27,7 @@
 		question: "We should prioritize citizenship to immigrants who can contribute to the economy.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "Nope nope my bro: let's not base human life on their economic worth. This would be agreeing that people are only worth as much money they can make.",
+		info: "Nope nope my bro: let's not base human life on their economic worth. This would be implying that people are only worth as much as the capital they can generate.",
   };
 	let six = {
 		question: "President Obama deported more immigrants than any other president to date",
@@ -45,7 +45,7 @@
 		question: "ICE and DHS have been around for a long time.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "The Department of Homeland Security was signed into law by President George W. Bush on November 25, 2002. ICE was established in March of 2003. The first step towards helping immigrants is to recognize that not all the current institutions have been around for a long time. Although the United States has a history of xenophobia, we need to stop normalizing the establishment of these new institutions, along with the long-standing ones."
+		info: "The Department of Homeland Security was signed into law by President George W. Bush on November 25, 2002. ICE was established in March of 2003. Although the United States has a history of xenophobia, we need to stop normalizing the establishment of these new institutions, along with the long-standing ones."
   };
 	let nine = {
 		question: "3,780 people in ICE custody have contracted COVID-19.",
@@ -63,6 +63,6 @@
 		question: "It's easy to gain citizenship the right way.",
 		answers: ["True", "False"],
 		correctAnswer: "False",
-		info: "To become a citizen, immigrants must first become a permanent resident (AKA green card holder) which is already very hard to get and can take a lot of time. Only after living in the US for five consecutive years as a green card holder may they apply for naturalization, which is the process of becoming a citizen. They must be 18 or older, be able to read, write, and speak basic English, understand the US constitution, have knowledge of the US history and government, and take the oath of allegiance to the US. Also to apply for naturalization they must pay a free of $725. This process can last 6-14 months. They must then complete an interview and test. The overall process is very very long and costly.",
+		info: "To become a citizen, immigrants must first become a permanent resident (AKA green card holder) which is already very hard to get and can take a lot of time. After living in the US for five consecutive years, a green card holder may then apply for naturalization, which is the process of becoming a citizen. They must be 18 or older, able to read, write, and speak basic English, understand the US constitution, have knowledge of the US history and government, and take the oath of allegiance to the US. The naturalizatioin fee is $725, and the process can last 6-14 months. They must then complete an interview and test. The overall process is very very long and costly.",
   };
 let questions = [one, two, three, four, five, six, seven, eight, nine, ten, eleven];
